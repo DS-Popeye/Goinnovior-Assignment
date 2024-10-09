e5fbe3734ba56c7e76fc96bf7061a8327b47d0f9
DS-Popeye <79585596+DS-Popeye@users.noreply.github.com>
2024-10-09 15:06:12 +0600

    Done

---
+++ Azuredream-Lodge/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {},
+  },
+  daisyui:{
+  theme:["light", "dark", "cupcake"],
+},
+  plugins: [],
+}
+
